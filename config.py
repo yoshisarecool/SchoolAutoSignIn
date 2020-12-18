9d156fc0eaac823d1cf8810e91748696b89b3218
yoshisarecool <76191379+yoshisarecool@users.noreply.github.com>
2020-12-17 21:14:16 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# FOR THIS THE ONLY OPTIONS ARE WINDOWS LINUX OR MAC
+
+OS = "WINDOWS"
+#OS = "LINUX"
+#OS = "MAC
+
+FCPS_EMAIL = "Student_id@fcpsschools.net"
+
+FCPS_PASSWORD = ""
+
+NAME = ""
+
+URL = ""
+
+TOPICS = ['ib chem', 'math', 'english', 'history', 'tech (testout)', "driver's ED", 'spanish']
+
+ENABLE_POSITIVE = True
+
+ENABLE_NEGATIVE = False
