cb747e0f27a335afea4742b5bc134646dad4ac50
yoshisarecool <76191379+yoshisarecool@users.noreply.github.com>
2020-12-17 21:11:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+import time
+import random
+from selenium import webdriver
+from datetime import datetime
+from config import FCPS_PASSWORD, FCPS_EMAIL, NAME, URL, TOPICS, ENABLE_NEGATIVE, ENABLE_POSITIVE, OS
+
+driver_location = None
+
+# Multi OS support lol very lazy ik
+if OS == "WINDOWS":
+    driver_location = 'driver/chromedriver.exe'
+elif OS == "MAC":
+    driver_location = 'driver/chromedriver.exe'
+elif OS == "LINUX":
+    driver_location = 'driver/chromedriver.exe'
+else:
+    print(f'NOT VALID OS OPTION\nYOU PUT "{OS}"')
+    exit(405)
+
+if driver_location is None:
+    print("ERROR COULD NOT SET DRIVER LOCATION!")
+    exit(406)
+
+while True:
+    x = datetime.today().strftime("%A")
+    y = datetime.today().strftime("%I:%M%p")
+    if x == "Monday" and y == f"08:31AM":
+        if ENABLE_NEGATIVE is True:
+            max1 = 2
+        else:
+            max1 = 1
+        if ENABLE_POSITIVE is True:
+            min1 = 0
+        else:
+            min1 = 1
+
+        if max1 == 1 and min1 == 1:
+            print("BOTH POSITIVE AND NEGATIVE ARE DISABLED THE ONLY VALID OPTION IS MIXED")
+
+        url = URL  # I got lazy to change url to URL so deal w/ it ok thx bye
+        feelings = random.randint(min1, max1)
+
+        random_topic_1 = random.choice(TOPICS)
+        random_topic_2 = random.choice(TOPICS)
+        while random_topic_1 == random_topic_2:
+            random_topic_2 = random.choice(TOPICS)
+
+        option = webdriver.ChromeOptions()
+        option.add_argument("--incognito")
+        browser = webdriver.Chrome(driver_location, options=option)
+        browser.get(f"{url}")
+        if browser.current_url != url and "https://accounts.google.com/signin/" in str(browser.current_url):
+            enter_email = browser.find_elements_by_class_name("whsOnd")
+            enter_email[0].send_keys(f"{FCPS_EMAIL}")
+            next_button = browser.find_elements_by_class_name("VfPpkd-RLmnJb")
+            next_button[0].click()
+            time.sleep(3)
+            enter_password = browser.find_elements_by_class_name("whsOnd")
+            enter_password[0].send_keys(f"{FCPS_PASSWORD}")
+            next_button = browser.find_elements_by_class_name("VfPpkd-RLmnJb")
+            next_button[0].click()
+            time.sleep(3)
+        if browser.current_url == url:
+            try:
+                text = f"Today I will be working on {random_topic_1}, and {random_topic_2} to get prepared for the week."
+                name_box = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
+                feeling_boxes = browser.find_elements_by_class_name("docssharedWizToggleLabeledLabelText")
+                what_am_doing_today = browser.find_elements_by_class_name("quantumWizTextinputPapertextareaInput")
+                submit = browser.find_elements_by_class_name("appsMaterialWizButtonPaperbuttonLabel")
+
+                name_box[0].send_keys(f"{NAME}")
+                feeling_boxes[feelings].click()
+                what_am_doing_today[0].send_keys(text)
+                submit[0].click()
+               #browser.close()
+                print(f"Submitted google form at:\nURL: {url}\nText: {text}\nFeeling Box (0 = Happy 1 = Mixed 2 = Bad): {feelings}")
+                exit(0)
+            except Exception as e:
+                print(e)
+                print("")
+                print("oh shit there was an error please send me a message w/ the error so ik how to fix LMAO")
+                print("Discord: Yousef#9999")
+                exit(404)
+        else:
+            print(f"ERROR! I am supposed to submit a Google Form to {url} but I was sent to {browser.current_url}")
+            exit(403)
+
+    time.sleep(60)
+
+
+# Exit Codes:
+# 404: Failed to submit form error: unknown
+# 403: After logging in google sent the browser to a diff url
+# 405: NOT SUPPORTED OS
+# 406: Could not set driver location
+#
+#
+# 1: It honestly depends I don't overwrite any codes I just make them
+# -1: Forcefully closed the program (ignore lol)
